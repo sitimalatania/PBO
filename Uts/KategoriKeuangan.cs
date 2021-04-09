234089047d9c7b6f10a6ea529254dd361943b7ec
sitimalatania <74101266+sitimalatania@users.noreply.github.com>
2021-04-10 00:34:26 +0700

    Add files via upload

---
+++ Uts/KategoriKeuangan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_dunno_what_im_doing
+{
+    class KategoriKeuangan
+    {
+        public void Kategori()
+        {
+            Console.WriteLine("Kategori Keuangan anda minggu ini");
+
+            TotalPengeluaran tp = new TotalPengeluaran();
+            uang money = new uang();
+            //K.masukkanbatasT();
+            int bts = money.Tu = tp.TotalUang;
+            Console.WriteLine(tp.TotalUang.ToString());
+            if (bts <= 5)
+            {
+                Console.Write("Anda Hemat");
+            }
+            else
+            {
+                Console.Write("Anda Boros");
+            }
+            Console.ReadKey();
+            Console.Clear();
+            PilihanTampilan p = new PilihanTampilan();
+            p.setpilihan();
+        }
+    }
+}
